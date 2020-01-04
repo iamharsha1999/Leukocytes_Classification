3223c4c7f002ca6ab3e477331429d0fb7715ae9c
iamharsha99 <ashharsha1999@gmail.com>
2020-01-04 21:59:26 +0530

    Modified the model architecture and made it shallow

--- Model/models.py
+++ Model/models.py
@@ -1,4 +1,4 @@
-from keras.layers import Dense, Conv2D, GlobalAveragePooling2D, MaxPooling2D, Dropout, Input, Activation, BatchNormalization, Flatten, GlobalMaxPooling2D, Add, SeparableConv2D
+from keras.layers import Dense, Conv2D, GlobalAveragePooling2D, MaxPooling2D, Dropout, Input, Activation, BatchNormalization, Flatten, GlobalMaxPooling2D, Add, SeparableConv2D, Flatten
 from keras.models import Sequential, Model
 from keras import applications
 from keras import backend as K
@@ -24,6 +24,10 @@ class DL_Model:
             x = base_model.output
             x = GlobalAveragePooling2D()(x)
             x = Dropout(0.7)(x)
+            x = Dense(32, activation = 'relu')(x)
+            x = BatchNormalization()(x)
+            x = Dense(16, activation = 'relu')(x)
+            x = BatchNormalization()(x)
             predictions = Dense(nc, activation= 'softmax')(x)
             model = Model(inputs = base_model.input, outputs = predictions)
 
@@ -53,25 +57,21 @@ class DL_Model:
             # x = Dropout(0.25)(x)
 
             # second CONV => RELU => CONV => RELU => POOL Block
-            x = Conv2D(128, (3, 3), padding = "same")(x)
-            x = Activation("swish")(x)
-            x = BatchNormalization(axis = channel_dim)(x)
-            x = Conv2D(128, (3, 3), padding = "same")(x)
-            x = Activation("swish")(x)
-            x = BatchNormalization(axis = channel_dim)(x)
             x = MaxPooling2D(pool_size = (2, 2))(x)
             # x = Dropout(0.25)(x)
 
             # first (and only) FC layer
-            x = GlobalMaxPooling2D()(x) # Change to GlobalMaxPooling2D
+            x = Flatten()(x) # Change to GlobalMaxPooling2D
             x = Dense(64, activation = 'swish')(x)
-            x = BatchNormalization(axis = channel_dim)(x)
+            x = BatchNormalization()(x)
+            x = Dropout(0.3)(x)
+
             x = Dense(32, activation = 'swish')(x)
             x = BatchNormalization()(x)
-            x = Dense(16, activation = 'swish')(x)
-            x = BatchNormalization()(x)
-            x = Dense(nc , activation = 'softmax')(x)
 
+
+
+            x = Dense(nc, activation = 'softmax')(x)
             model  = Model(inputs=inpt, outputs = x)
 
             return model
--- train.py
+++ train.py
@@ -1,10 +1,9 @@
-
 from Model import DL_Model, AdaBound
 from keras.callbacks import ModelCheckpoint
 from keras.utils import to_categorical
 from keras.utils.generic_utils import get_custom_objects
 from keras.layers import Activation
-from keras.preprocessing.image import ImageDataGenerator
+from keras.optimizers import SGD
 from keras import backend as K
 import  numpy as np
 import matplotlib.pyplot as plt
@@ -26,47 +25,38 @@ x_val = np.load('Dataset/Image_Classification/Numpy Files/x_val.npy')
 y_train = np.load('Dataset/Image_Classification/Numpy Files/y_train.npy')
 y_val = np.load('Dataset/Image_Classification/Numpy Files/y_val.npy')
 
+x_train = (x_train.astype(np.float32))/255
+x_val = (x_val.astype(np.float32))/255
+
 y_train = to_categorical(y_train, num_classes = 5)
 y_val   = to_categorical(y_val, num_classes = 5)
 
 num_classes = 5
 
-## Data Augmentation
-datagen = ImageDataGenerator(featurewise_center = True, rotation_range= 45, horizontal_flip=True, vertical_flip = True, brightness_range=[0.2,1.0])
-datagen.fit(np.concatenate([x_train,x_val]))
-
-
 
 # # AdaBound Optimizer
 # optm = AdaBound(lr=1e-03, final_lr=0.1, gamma=1e-03, weight_decay=0., amsbound=False)
-# Build the Model
 
+
+# Build the Model
+## SGD Optmizer
+sgd = SGD(lr = 1e-6, momentum = 0.9)
 model = DL_Model.build_feature_model(300, 300, 3, num_classes, 'smallvgg16')
-model.compile(loss = 'categorical_crossentropy', optimizer  = 'adam', metrics = ['accuracy'])
+model.compile(loss = 'categorical_crossentropy', optimizer  = 'sgd', metrics = ['accuracy'])
 model.summary()
 
 
 # Train the Model
 epochs = 100
-batch_size = 8
+batch_size = 16
 
-model.fit_generator(datagen.flow(x_train, y_train, batch_size = batch_size),epochs = 100, steps_per_epoch = len(x_train)/batch_size, validation_data = (x_val,y_val))
+# model.fit_generator(datagen.flow(x_train, y_train, batch_size = batch_size),epochs = 100, steps_per_epoch = len(x_train)/batch_size, validation_data = (x_val,y_val))
 
 # Create Checkpoints
 weights = 'Dataset/Image_Classification/Weights/SmallVGG_Fully_Swish/weights-{epoch:02d}-{val_acc:.2f}.hdf5'
-checkpoints = ModelCheckpoint(weights, verbose= 1, monitor= 'val_acc', save_best_only= False, mode = 'auto')
+checkpoints = ModelCheckpoint(weights, verbose= 1, monitor= 'val_acc', save_best_only= True, mode = 'auto')
 callbacks = [checkpoints]
-
-for e in range(epochs):
-    print('Epoch', e+1)
-    batches = 0
-    for x_batch, y_batch in datagen.flow(x_train, y_train, batch_size=32):
-        history = model.fit(x_batch,y_batch, verbose = 1, callbacks = callbacks,validation_data = (x_val, y_val))
-        batches += 1
-        if batches >= len(x_train) / batch_size:
-            break
-
-
+history = model.fit(x_train,y_train, verbose = 1, callbacks = callbacks,epochs = epochs,batch_size = batch_size, validation_data = (x_val, y_val))
 
 
 

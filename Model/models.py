f45b1f046dfe58bed33c3440365516f4469ab7a4
Harsha <ashharsha1999@gmail.com>
2020-01-23 12:07:14 +0530

    Added Densenet and Hegde CNN

--- Model/models.py
+++ Model/models.py
@@ -34,6 +34,20 @@ class DL_Model:
 
             return model
 
+        elif model == 'densenet':
+
+            base_model = applications.densenet.DenseNet121(weights = None, include_top = False, input_shape = input_shape)
+            x = base_model.output
+            x = GlobalAveragePooling2D()(x)
+            x = Dense(32, activation = "relu")(x)
+            x = BatchNormalization()(x)
+            x = Dense(16, activation = "relu")(x)
+            x = BatchNormalization()(x)
+            predictions = Dense(nc, activation = 'softmax')(x)
+            model = Model(inputs = base_model.input, outputs = predictions)
+
+            return model
+
         ## Build the SmallVGG16
         elif model == 'smallvgg16':
 
@@ -41,35 +55,30 @@ class DL_Model:
             # first (and only) CONV => RELU => POOL block
             inpt = Input(shape = input_shape)
             x = Conv2D(32, (3, 3), padding = "same")(inpt)
-            x = Activation("swish")(x)
+            x = Activation("relu")(x)
             x = BatchNormalization(axis = channel_dim)(x)
             x = MaxPooling2D(pool_size = (3, 3))(x)
-            # x = Dropout(0.25)(x)
 
             # first CONV => RELU => CONV => RELU => POOL block
             x = Conv2D(64, (3, 3), padding = "same")(x)
-            x = Activation("swish")(x)
+            x = Activation("relu")(x)
             x = BatchNormalization(axis = channel_dim)(x)
             x = Conv2D(64, (3, 3), padding = "same")(x)
-            x = Activation("swish")(x)
+            x = Activation("relu")(x)
             x = BatchNormalization(axis = channel_dim)(x)
             x = MaxPooling2D(pool_size = (2, 2))(x)
-            # x = Dropout(0.25)(x)
 
             # second CONV => RELU => CONV => RELU => POOL Block
             x = MaxPooling2D(pool_size = (2, 2))(x)
-            # x = Dropout(0.25)(x)
 
             # first (and only) FC layer
-            x = Flatten()(x) # Change to GlobalMaxPooling2D
-            x = Dense(64, activation = 'swish')(x)
+            x = Flatten()(x)
+            x = Dense(64, activation = 'relu')(x)
             x = BatchNormalization()(x)
             x = Dropout(0.3)(x)
 
-            x = Dense(32, activation = 'swish')(x)
+            x = Dense(32, activation = 'relu')(x)
             x = BatchNormalization()(x)
-
-
 
             x = Dense(nc, activation = 'softmax')(x)
             model  = Model(inputs=inpt, outputs = x)
@@ -109,53 +118,38 @@ class DL_Model:
 
             return model
 
-        elif model == 'smallxception':
+        elif model == 'hegde_cnn':
 
             inpt = Input(shape = input_shape)
 
-            ## Entry Flow
-            x = Conv2D(32,3,padding='same')(inpt)
-            x = BatchNormalization()(x)
-            x = Activation('swish')(x)
+            # Layer 1
+            x = Conv2D(96, (11, 11), strides = 4, padding = "valid")(inpt)
+            x = Activation("relu")(x)
+            x = MaxPooling2D(pool_size = (3,3), strides = 2)
+
+            # Layer 2
+            x = Conv2D(256, (5,5), strides = 1, padding = "same")(x)
+            x = Activation("relu")(x)
+            x = MaxPooling2D(pool_size = (3,3), strides = 2)
+
+            #Layer 3
+            x = Conv2D(384, (3,3), strides = 1, padding = "same")(x)
+            x = Activation("relu")(x)
+            x = MaxPooling2D(pool_size = (3,3), padding = "same")(x)
+
+            #Layer 4
+            x = Conv2D(384, (3,3), strides = 1, padding = "same")(x)
+            x = Activation("relu")(x)
 
 
-            for size in [128,256] :
+            # Layer 5
+            x = Conv2D(256, (3,3), strides = 1, padding = "same")(x)
+            x = Activation("relu")(x)
+            x = MaxPooling2D(pool_size = (3,3), padding = "same")(x)
 
-                x = Activation('swish')(x)
-                x = Conv2D(size, 3, padding='same')(x)
-                x = BatchNormalization()(x)
-                x = MaxPooling2D(3, strides=2, padding='same')(x)
+            # Fully Connected Layer
+            x = Dense(500, activation = "relu")(x)
+            x = Dense(256, activation = "relu")(x)
+            x = Dense(5, activation = "softmax")(x)
 
-            num_blocks = 1
-
-            ## Middle Block
-            for _ in range(num_blocks) :
-
-                x = Activation('swish')(x)
-                x = Conv2D(256, 3, padding='same')(x)
-                x = BatchNormalization()(x)
-
-
-            ## Exit Flow
-            x = Activation('swish')(x)
-            x = Conv2D(256, 3, padding='same')(x)
-            x = BatchNormalization()(x)
-            x = MaxPooling2D(3, strides=2, padding='same')(x)
-
-
-            x = Activation('swish')(x)
-            x = Conv2D(512, 3, padding='same')(x)
-            x = BatchNormalization()(x)
-
-            x = GlobalAveragePooling2D()(x)
-            x = Dense(64, activation = 'swish')(x)
-            x = BatchNormalization(axis = channel_dim)(x)
-            x = Dense(32, activation = 'swish')(x)
-            x = BatchNormalization()(x)
-            x = Dense(16, activation = 'swish')(x)
-            x = BatchNormalization()(x)
-            predictions = Dense(nc, activation='softmax')(x)
-
-            model = Model(inputs = inpt, outputs = predictions)
-
-            return model
+    
